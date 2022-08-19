c20ad67dd9561c7c4605357c20f0c09fcb262a61
Ralphine <r.deroo99@gmail.com>
2022-08-19 11:56:31 +0200

    SPR Rolles back importing Vidaloka font to fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'vidaloka': ['"Vidaloka"', 'serif'],
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
